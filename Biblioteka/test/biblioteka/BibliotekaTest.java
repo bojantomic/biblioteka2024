935959f1a615a7cef24c8c3165f61f108f214d78
bojantomic <tomicb@fon.rs>
2024-03-11 15:54:42 +0100

    Testirana Biblioteka i mock VremenskiServis

--- Biblioteka/test/biblioteka/BibliotekaTest.java
+++ Biblioteka/test/biblioteka/BibliotekaTest.java
@@ -1,13 +1,27 @@
 package biblioteka;
 
+import java.time.LocalDateTime;
+
+import org.easymock.EasyMock;
+import org.easymock.EasyMockExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import biblioteka.interfejs.BibliotekaInterfejs;
 import biblioteka.interfejs.BibliotekaInterfejsTest;
+import util.VremenskiServis;
 
+@ExtendWith(EasyMockExtension.class)
 class BibliotekaTest extends BibliotekaInterfejsTest {
 
 	@Override
 	public BibliotekaInterfejs getInstance() {
-		return new Biblioteka();
+		VremenskiServis s = EasyMock.mock(VremenskiServis.class);
+		
+		EasyMock.expect(s.vratiVreme("Beograd")).andReturn(LocalDateTime.now()).anyTimes();
+		
+		EasyMock.replay(s);
+		
+		return new Biblioteka(s);
 	}
 
 }
