78dbd822ac9cd7e1b2aa6031941257baf715962f
bojantomic <tomicb@fon.rs>
2024-03-18 13:54:13 +0100

    Dokumentovana klasa Knjiga

--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -3,36 +3,91 @@ package biblioteka;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Predstavlja elektronsku ili stampanu knjigu.
+ * 
+ * Moze imati autora, naslov, izdavaca, izdanje i isbn broj.
+ * 
+ * @author Bojan Tomic
+ * @since 0.1.0
+ */
 public class Knjiga {
 	
+	/**
+	 * Naslov knjige kao String.
+	 */
 	private String naslov;
 	
+	/**
+	 * Isbn broj knjige koji bi trebalo da ima 9 ili 13 cifara
+	 */
 	private long isbn;
 	
+	/**
+	 * Lista autora knjige.
+	 * 
+	 * Lista moze biti i prazna ili null u slucajevima da
+	 * je autor knjige nepoznat (npr. narodne pesme ili narodne price)
+	 */
 	private List<Autor> autori;
 	
+	/**
+	 * Naziv izdavaca knjige kao String
+	 */
 	private String izdavac;
-	
+
+	/**
+	 * Izdanje knjige kao ceo broj
+	 */
 	private int izdanje;
 
+	/**
+	 * Vraca naslov knjige
+	 * 
+	 * @return naslov knjige kao String
+	 */
 	public String getNaslov() {
 		return naslov;
 	}
 
+	/**
+	 * Postavlja vrednost atributa naslov.
+	 * 
+	 * Naslov ne sme biti null niti prazan String.
+	 * 
+	 * @param naslov nova vrednost za naslov knjige
+	 * 
+	 * @throws NullPointerException ako se unese null vrednost za naslov
+	 * @throws IllegalArgumentException ako se unese prazan String kao naslov
+	 */
 	public void setNaslov(String naslov) {
 		if (naslov == null)
 			throw new NullPointerException("Naslov ne sme biti null");
 		
 		if (naslov.isEmpty())
-			throw new IllegalArgumentException("Naslov ne me biti prazan");
+			throw new IllegalArgumentException("Naslov ne sme biti prazan");
 		
 		this.naslov = naslov;
 	}
 
+	/**
+	 * Vraca isbn knjige
+	 * 
+	 * @return isbne knjige kao ceo broj
+	 */
 	public long getIsbn() {
 		return isbn;
 	}
 
+	/**
+	 * Postavlja vrednost za isbn broj knjige.
+	 * 
+	 * ISBN broj mora biti veci od nule.
+	 * 
+	 * @param isbn isbn broj knjige kao ceo broj koji mora imati 10-13 cifara
+	 * @throws IllegalArgumentException 
+	 * ako se unese isbn broj koji ima manje od 10 ili vise od 13 cifara
+	 */
 	public void setIsbn(long isbn) {
 		if (isbn < 1000000000 || isbn >  9999999999999L)
 			throw new IllegalArgumentException("ISBN mora imati bar 10 cifara");
@@ -40,31 +95,77 @@ public class Knjiga {
 		this.isbn = isbn;
 	}
 
+	/**
+	 * Vraca listu autora knjige.
+	 * 
+	 * @return Listu sa autorima knjige, ili null ako knjiga nema poznate autore.
+	 */
+	public List<Autor> getAutori() {
+		return autori;
+	}
+
+	/**
+	 * Postavlja listu sa autorima knjige.
+	 * 
+	 * Lista sme biti null ili prazna ako knjiga nema poznate autore.
+	 * 
+	 * @param autori lista sa autorima knjige.
+	 */
+	public void setAutori(List<Autor> autori) {
+		this.autori = autori;
+	}
+
+	/**
+	 * Vraca naziv izdavacke kuce koja je izdala knjigu.
+	 * 
+	 * @return Naziv izdavacke kuce za tu knjigu kao String
+	 */
 	public String getIzdavac() {
 		return izdavac;
 	}
 
+	/**
+	 * Postavlja vrednost atributa izdavac.
+	 * 
+	 * Izdavac ne sme biti null niti prazan String.
+	 * 
+	 * @param izdavac nova vrednost za naziv izdavaca knjige
+	 * 
+	 * @throws NullPointerException ako se unese null vrednost za izdavaca
+	 * @throws IllegalArgumentException ako se unese prazan String kao izdavac
+	 */
 	public void setIzdavac(String izdavac) {
+		if (izdavac == null)
+			throw new NullPointerException("Izdavac ne sme biti null");
+		
+		if (izdavac.isEmpty())
+			throw new IllegalArgumentException("Izdavac ne sme biti prazan");		
+		
 		this.izdavac = izdavac;
 	}
 
+	/**
+	 * Vraca izdanje knjige
+	 * 
+	 * @return izdanje knjige kao ceo broj.
+	 */
 	public int getIzdanje() {
 		return izdanje;
 	}
 
+	/**
+	 * Postavlja vrednost za izdanje knjige.
+	 * 
+	 * Broj izdanja mora biti veci od nule.
+	 * 
+	 * @param izdanje broj izdanja kao ceo broj
+	 * @throws IllegalArgumentException ako se unese broj izdanja koji je manji od 1
+	 */
 	public void setIzdanje(int izdanje) {
 		if (izdanje < 1)
 			throw new IllegalArgumentException("Izdanje mora biti 1 ili vece");
 		
 		this.izdanje = izdanje;
-	}
-
-	public List<Autor> getAutori() {
-		return autori;
-	}
-
-	public void setAutori(List<Autor> autori) {
-		this.autori = autori;
 	}
 
 	@Override
@@ -73,11 +174,25 @@ public class Knjiga {
 				+ ", izdanje=" + izdanje + "]";
 	}
 
+	/**
+	 * Izracunava hash code na osnovu isbn broja.
+	 * 
+	 * @return hash code na osnovu isbn broja.
+	 */
 	@Override
 	public int hashCode() {
 		return Objects.hash(isbn);
 	}
 
+	/**
+	 * Poredi dve knjige preko isbn broja.
+	 * 
+	 * @return
+	 * <ul>
+	 * 		<li> true - ako je isbn broj obe knjige isti ili ako je unet isti objekat </li>
+	 * 		<li> false - ako je unet null objekat ili ako nije klase Knjiga </li>
+	 * </ul>
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -89,5 +204,6 @@ public class Knjiga {
 		Knjiga other = (Knjiga) obj;
 		return isbn == other.isbn;
 	}
-
+	
+	
 }
